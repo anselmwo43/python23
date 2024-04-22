6f5c80fc1575952e5a96305d650bd6c3db50bbc3
Yared <jaredyared83@gmail.com>
2024-04-22 20:26:28 +0330

    reference and copy of a dict

--- mini_projects/list_of_users.py
+++ mini_projects/list_of_users.py
@@ -1,14 +1,64 @@
+# reference of a dict and copy of a dict
+new_dict = {
+    "name": "hello",
+}
+
+second_dict = new_dict.copy()
+
+new_dict["name"] = "bye"
+
+print(second_dict["name"])
+
+
 user = {
     "name": "Yared",
-    "age": "23",
+    "age": 23,
     "username": "yared",
     "email": "jaredyared83@gmail.com"
 }
 
 users = []
 names = ["Yared", "Sinidu", "Helina", "Abebe", "Kebede"]
+"""
+[{
+    "name": "Yared",
+    "age": "23",
+    "username": "yared",
+    "email": "jaredyared83@gmail.com"
+},
+{
+    "name": "Yared",
+    "age": "23",
+    "username": "yared",
+    "email": "jaredyared83@gmail.com"
+},
+{
+    "name": "Yared",
+    "age": "23",
+    "username": "yared",
+    "email": "jaredyared83@gmail.com"
+},
+{
+    "name": "Yared",
+    "age": "23",
+    "username": "yared",
+    "email": "jaredyared83@gmail.com"
+},
+{
+    "name": "Yared",
+    "age": "23",
+    "username": "yared",
+    "email": "jaredyared83@gmail.com"
+}]
+"""
 
 for i in range(5):
-    users.append(user)
+    users.append(user.copy())
+
+
+
+for user, name in zip(users, names):
+    print(user, name)
+    user["name"] = name
 
 print(users)
