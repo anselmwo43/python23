8b1767b272b75b84ca2671ba5b7ca054f5f8ad1c
Yared <jaredyared83@gmail.com>
2024-04-22 19:57:10 +0330

    different names

---
+++ mini_projects/list_of_users.py
@@ -0,0 +1,14 @@
+user = {
+    "name": "Yared",
+    "age": "23",
+    "username": "yared",
+    "email": "jaredyared83@gmail.com"
+}
+
+users = []
+names = ["Yared", "Sinidu", "Helina", "Abebe", "Kebede"]
+
+for i in range(5):
+    users.append(user)
+
+print(users)
