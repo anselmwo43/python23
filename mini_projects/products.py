1006e0740146067cf075121bed6940175a1919ac
Yared <jaredyared83@gmail.com>
2024-04-25 21:25:37 +0330

    display products

--- mini_projects/products.py
+++ mini_projects/products.py
@@ -3,18 +3,20 @@ products = []
 def sell(product):
     pass
 
-def restock(product, amount, unit):
+def restock(product, amount):
     pass
 
 def displayProducts():
     # Product name: Sugar
     # price: 80 birr/Kg
-    pass
+    for product in products:
+        print(f"Product name: {product["name"]}\nPrice: {product["price"]} {product["currency"]}/{product["unit"]}")
+        print()
 
 def createProduct(*, name:str, price:float, unit:str, currency:str):
     product = {"name": name, "price": price, "unit": unit, "currency": currency}
     products.append(product)
-    print(products)
 
 createProduct(name="Sugar", price=90.5, unit="Kg", currency="birr")
 createProduct(name="Jordan Shoe", price=3000, unit="piece", currency="birr")
+displayProducts()
